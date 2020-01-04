d474b2a901137833e283f029934138ccaddaa9f0
harshithadega <37622329+harshithadega@users.noreply.github.com>
2020-01-04 00:05:30 -0800

    Add files via upload

--- EmpUtils.java
+++ EmpUtils.java
@@ -5,4 +5,7 @@ public class EmpUtils {
 	public static final String GETEMPLS = "select * from emp";
 	public static final String GETEMPlBYCODE = "select * from emp where empno=?";
 	public static final String DELROW = "delete from emp where empno =?";
+	public static final String UPDSAL="update emp set sal=? where empno =?";
+	public static final String UPDALL="update emp set ename=?,job=?,mgr=?,hiredate=?,sal=?,comm=?,deptno=? where empno=?";
+	
 }
