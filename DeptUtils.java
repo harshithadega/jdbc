2efb59a9486da35072b3654512f7f65097816f19
harshithadega <37622329+harshithadega@users.noreply.github.com>
2020-01-04 03:18:57 -0800

    Add files via upload

---
+++ DeptUtils.java
@@ -0,0 +1,7 @@
+package com.deloitte.dept.utils;
+
+public class DeptUtils {
+	public static String insertqry="insert into dept values(?,?,?)";
+	public static final String UPDALL="update dept set dname=?,loc=? where deptno =?";
+	public static final String GETDEPTS = "select * from dept";
+}
